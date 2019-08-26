d328ec5c41fe1089e878fd7f871bea66ed9bc0db
Awea <aweaoftheworld@gmail.com>
2019-08-26 16:15:10 +0200

    :fire: Remove useless node-notifier.
    Because there is already a notification inside the terminal and in the browser.

--- webpack/dev.js
+++ webpack/dev.js
@@ -2,7 +2,6 @@ const merge = require("webpack-merge");
 const common = require("./common.js");
 const webpack = require("webpack");
 const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
-const notifier = require("node-notifier");
 const path = require("path");
 const utils = require("./utils");
 
@@ -38,19 +37,6 @@ module.exports = merge(common, {
     new FriendlyErrorsPlugin({
       compilationSuccessInfo: {
         messages: ["Your application is running here: http://localhost:3000"]
-      },
-      onErrors: (severity, errors) => {
-        if (severity !== "error") return;
-
-        const error = errors[0];
-        const filename = error.file && error.file.split("!").pop();
-
-        notifier.notify({
-          title: "SPA-VUE",
-          message: severity + ": " + error.name,
-          subtitle: filename || "",
-          icon: path.join(__dirname, "logo.png")
-        });
       }
     }),
     utils.definePlugin()
